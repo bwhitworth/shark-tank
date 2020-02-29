c7762007e21d322bbfd6d6e9ae4c6c66cf51f8f9
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-29 10:32:06 -0600

    prints Tank with names

--- javascripts/components/tank.js
+++ javascripts/components/tank.js
@@ -1,7 +1,19 @@
 import utils from "../helpers/utils.js";
+import personData from "../helpers/data/personData.js";
 
 const tankBuilder = () => {
-  let domString = 'help';
+  const persons = personData.getAlivePersons();
+  let domString = '';
+  domString += '<div class="col-6">'
+  domString += '  <div class="card">',
+  domString += '    <div class="card-header">TANK</div>',
+  domString += '    <ul class="list-group list-group-flush">',
+  persons.forEach((p) => {
+    domString += `  <li class="list-group-item">${p.name}</li>`
+  });
+  domString += '    </ul>',
+  domString += '  </div>',
+  domString += '</div>'
   utils.printToDom('tank', domString);
 };
 
