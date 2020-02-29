65843994483c3c43ca811bec08ac796a27405f96
Bethany Whitworth <bethanyjwhitworth@gmail.com>
2020-02-29 11:30:35 -0600

    shark attack works

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,17 @@
 import tank from "./components/tank.js";
 import graveyard from "./components/graveyard.js";
+import personData from "./helpers/data/personData.js";
+
+const sharkAttackEvent = () => {
+  personData.randomMurder();
+  tank.tankBuilder();
+  graveyard.graveyardBuilder();
+};
 
 const init = () => {
   tank.tankBuilder();
   graveyard.graveyardBuilder();
+  $( "#bite-me" ).click(sharkAttackEvent);
 };
 
 init();
